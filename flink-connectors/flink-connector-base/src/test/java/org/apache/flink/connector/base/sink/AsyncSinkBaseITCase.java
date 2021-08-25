5401154aad327c8fc84465032c22e10f7a00e47e
Zichen Liu <zichel@amazon.com>
2021-08-25 15:37:51 +0100

    Added failure integration test case

--- flink-connectors/flink-connector-base/src/test/java/org/apache/flink/connector/base/sink/AsyncSinkBaseITCase.java
+++ flink-connectors/flink-connector-base/src/test/java/org/apache/flink/connector/base/sink/AsyncSinkBaseITCase.java
@@ -1,5 +1,11 @@
 package org.apache.flink.connector.base.sink;
 
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.ExecutionOptions;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -8,15 +14,38 @@ import org.junit.Test;
 /** Tests Async Sink. */
 public class AsyncSinkBaseITCase {
 
+    private class MapWithOccasionalFailure extends RichMapFunction<Long, String> {
+
+        private final boolean simulateFailures;
+
+        public MapWithOccasionalFailure(boolean simulateFailures){
+            this.simulateFailures = simulateFailures;
+        }
+
+        @Override
+        public String map(Long value) throws Exception {
+            System.out.println(getRuntimeContext().getAttemptNumber());
+            if (getRuntimeContext().getIndexOfThisSubtask() == 0 &&
+                    getRuntimeContext().getAttemptNumber() == 0 && simulateFailures) {
+                throw new RuntimeException("An intentional error occurred");
+            }
+            return value.toString();
+        }
+    }
+
     @Test
     public void writeSingleDataToSink() throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(20);
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(1, Time.milliseconds(200)));
 
-        DataStream<String> source = env.fromSequence(0, 10).map(Object::toString);
-
-        source.sinkTo(new ArrayListAsyncSink());
+        env
+                .fromSequence(0, 100)
+                .map(new MapWithOccasionalFailure(true))
+                .sinkTo(new ArrayListAsyncSink());
 
         env.execute("Integration Test: AsyncSinkBaseITCase");
     }
+
+
 }
