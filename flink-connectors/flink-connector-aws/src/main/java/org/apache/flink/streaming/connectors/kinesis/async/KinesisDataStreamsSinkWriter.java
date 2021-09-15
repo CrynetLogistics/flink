059dd4804a100acea4a170365534401b76f0d655
Zichen Liu <zichel@amazon.com>
2021-09-15 18:02:44 +0100

    [FLINK-24227][connectors] Added Kinesis Data Streams Sink into a new module flink-connectors/flink-connector-kinesis-async

---
+++ flink-connectors/flink-connector-aws/src/main/java/org/apache/flink/streaming/connectors/kinesis/async/KinesisDataStreamsSinkWriter.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.connectors.kinesis.async;
+
+import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.api.connector.sink.Sink;
+import org.apache.flink.connector.base.sink.writer.AsyncSinkWriter;
+import org.apache.flink.connector.base.sink.writer.ElementConverter;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.metrics.MetricGroup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsRequest;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsRequestEntry;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsResponse;
+import software.amazon.awssdk.services.kinesis.model.PutRecordsResultEntry;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+/**
+ * Sink writer created by {@link KinesisDataStreamsSink} to write to Kinesis Data Streams. More
+ * details on the operation of this sink writer may be found in the doc for {@link
+ * KinesisDataStreamsSink}. More details on the internals of this sink writer may be found in {@link
+ * AsyncSinkWriter}.
+ *
+ * <p>The {@link KinesisAsyncClient} used here may be configured in the standard way for the AWS SDK
+ * 2.x. e.g. the provision of {@code AWS_REGION}, {@code AWS_ACCESS_KEY_ID} and {@code
+ * AWS_SECRET_ACCESS_KEY} through environment variables etc.
+ */
+@PublicEvolving
+public class KinesisDataStreamsSinkWriter<InputT>
+        extends AsyncSinkWriter<InputT, PutRecordsRequestEntry> {
+
+    private static final String TOTAL_FULLY_SUCCESSFUL_FLUSHES_METRIC =
+            "totalFullySuccessfulFlushes";
+    private static final String TOTAL_PARTIALLY_SUCCESSFUL_FLUSHES_METRIC =
+            "totalPartiallySuccessfulFlushes";
+    private static final String TOTAL_FULLY_FAILED_FLUSHES_METRIC = "totalFullyFailedFlushes";
+    private static final String TOTAL_FAILED_ELEMENTS_METRIC = "totalFailedElements";
+    private transient Counter totalFullySuccessfulFlushesCounter;
+    private transient Counter totalPartiallySuccessfulFlushesCounter;
+    private transient Counter totalFullyFailedFlushesCounter;
+    private transient Counter totalFailedElementsCounter;
+
+    private final String streamName;
+    private final MetricGroup metrics;
+    private static final KinesisAsyncClient client = KinesisAsyncClient.create();
+    private static final Logger LOG = LoggerFactory.getLogger(KinesisDataStreamsSinkWriter.class);
+
+    KinesisDataStreamsSinkWriter(
+            ElementConverter<InputT, PutRecordsRequestEntry> elementConverter,
+            Sink.InitContext context,
+            int maxBatchSize,
+            int maxInFlightRequests,
+            int maxBufferedRequests,
+            long flushOnBufferSizeInBytes,
+            long maxTimeInBufferMS,
+            String streamName) {
+        super(
+                elementConverter,
+                context,
+                maxBatchSize,
+                maxInFlightRequests,
+                maxBufferedRequests,
+                flushOnBufferSizeInBytes,
+                maxTimeInBufferMS);
+        this.streamName = streamName;
+        this.metrics = context.metricGroup();
+        initMetricsGroup();
+    }
+
+    @Override
+    protected void submitRequestEntries(
+            List<PutRecordsRequestEntry> requestEntries,
+            Consumer<Collection<PutRecordsRequestEntry>> requestResult) {
+
+        PutRecordsRequest batchRequest =
+                PutRecordsRequest.builder().records(requestEntries).streamName(streamName).build();
+
+        LOG.trace("Request to submit {} entries to KDS using KDS Sink.", requestEntries.size());
+
+        CompletableFuture<PutRecordsResponse> future = client.putRecords(batchRequest);
+
+        future.whenComplete(
+                (response, err) -> {
+                    if (err != null) {
+                        LOG.warn(
+                                "KDS Sink failed to persist {} entries to KDS, retrying whole batch",
+                                requestEntries.size());
+                        totalFullyFailedFlushesCounter.inc();
+                        totalFailedElementsCounter.inc(requestEntries.size());
+
+                        requestResult.accept(requestEntries);
+                        return;
+                    }
+
+                    if (response.failedRecordCount() > 0) {
+                        LOG.warn(
+                                "KDS Sink failed to persist {} entries to KDS, retrying a partial batch",
+                                response.failedRecordCount());
+                        totalPartiallySuccessfulFlushesCounter.inc();
+                        totalFailedElementsCounter.inc(response.failedRecordCount());
+
+                        ArrayList<PutRecordsRequestEntry> failedRequestEntries =
+                                new ArrayList<>(response.failedRecordCount());
+                        List<PutRecordsResultEntry> records = response.records();
+
+                        for (int i = 0; i < records.size(); i++) {
+                            if (records.get(i).errorCode() != null) {
+                                failedRequestEntries.add(requestEntries.get(i));
+                            }
+                        }
+
+                        requestResult.accept(failedRequestEntries);
+                    } else {
+                        totalFullySuccessfulFlushesCounter.inc();
+                        requestResult.accept(Collections.emptyList());
+                    }
+                });
+    }
+
+    @Override
+    protected long getSizeInBytes(PutRecordsRequestEntry requestEntry) {
+        return requestEntry.data().asByteArrayUnsafe().length;
+    }
+
+    private void initMetricsGroup() {
+        totalFullySuccessfulFlushesCounter = metrics.counter(TOTAL_FULLY_SUCCESSFUL_FLUSHES_METRIC);
+        totalPartiallySuccessfulFlushesCounter =
+                metrics.counter(TOTAL_PARTIALLY_SUCCESSFUL_FLUSHES_METRIC);
+        totalFullyFailedFlushesCounter = metrics.counter(TOTAL_FULLY_FAILED_FLUSHES_METRIC);
+        totalFailedElementsCounter = metrics.counter(TOTAL_FAILED_ELEMENTS_METRIC);
+    }
+}
---
+++ flink-connectors/flink-connector-aws/src/test/java/org/apache/flink/streaming/connectors/kinesis/async/testutils/KinesaliteContainer.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.connectors.kinesis.async.testutils;
+
+import org.rnorth.ducttape.unreliables.Unreliables;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.AbstractWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.http.SdkHttpConfigurationOption;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
+import software.amazon.awssdk.services.kinesis.model.ListStreamsResponse;
+import software.amazon.awssdk.utils.AttributeMap;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A {@code org.testcontainers} based on Kinesalite.
+ *
+ * <p>Note that the more obvious localstack container with Kinesis took 1 minute to start vs 3
+ * seconds of Kinesalite.
+ */
+public class KinesaliteContainer extends GenericContainer<KinesaliteContainer> {
+
+    private static final int KINESALITE_PORT = 4567;
+    private static final String ACCESS_KEY = "access key";
+    private static final String SECRET_KEY = "secret key";
+    private static final Region REGION = Region.US_EAST_1;
+    private static final String AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID";
+    private static final String AWS_SECRET_KEY = "AWS_SECRET_KEY";
+
+    public KinesaliteContainer(DockerImageName imageName) {
+        super(imageName);
+
+        withEnv(AWS_ACCESS_KEY_ID, ACCESS_KEY);
+        withEnv(AWS_SECRET_KEY, SECRET_KEY);
+        withExposedPorts(KINESALITE_PORT);
+        waitingFor(new ListStreamsWaitStrategy());
+        withCreateContainerCmdModifier(
+                cmd ->
+                        cmd.withEntrypoint(
+                                "/tini",
+                                "--",
+                                "/usr/src/app/node_modules/kinesalite/cli.js",
+                                "--path",
+                                "/var/lib/kinesalite",
+                                "--ssl"));
+    }
+
+    public String getHostEndpointUrl() {
+        return String.format("https://%s:%s", getHost(), getMappedPort(KINESALITE_PORT));
+    }
+
+    public String getAccessKey() {
+        return ACCESS_KEY;
+    }
+
+    public String getSecretKey() {
+        return SECRET_KEY;
+    }
+
+    public Region getRegion() {
+        return REGION;
+    }
+
+    public KinesisAsyncClient getNewClient() throws URISyntaxException {
+        return KinesisAsyncClient.builder()
+                .endpointOverride(new URI(getHostEndpointUrl()))
+                .region(REGION)
+                .credentialsProvider(
+                        () -> AwsBasicCredentials.create(getAccessKey(), getSecretKey()))
+                .httpClient(buildSdkAsyncHttpClient())
+                .build();
+    }
+
+    private class ListStreamsWaitStrategy extends AbstractWaitStrategy {
+        @Override
+        protected void waitUntilReady() {
+            Unreliables.retryUntilSuccess(
+                    (int) this.startupTimeout.getSeconds(),
+                    TimeUnit.SECONDS,
+                    () -> this.getRateLimiter().getWhenReady(this::tryList));
+        }
+
+        private ListStreamsResponse tryList()
+                throws URISyntaxException, ExecutionException, InterruptedException {
+            return getNewClient().listStreams().get();
+        }
+    }
+
+    private SdkAsyncHttpClient buildSdkAsyncHttpClient() {
+        return NettyNioAsyncHttpClient.builder()
+                .buildWithDefaults(
+                        AttributeMap.builder()
+                                .put(SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES, true)
+                                .build());
+    }
+}
