ecd5156f55da69f041061ce7ffb746d02efadf0e
Zichen Liu <zichel@amazon.com>
2021-09-16 10:29:37 +0100

    [FLINK-24227][connectors] more testing

---
+++ flink-connectors/flink-connector-kinesis-async/src/test/java/org/apache/flink/streaming/connectors/kinesis/async/KinesisDataStreamsAsyncSinkITCase.java
@@ -0,0 +1,86 @@
+package org.apache.flink.streaming.connectors.kinesis.async;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+/** a. */
+public class KinesisDataStreamsAsyncSinkITCase {
+
+    @Test
+    public void test() throws Exception {
+        // set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // env.enableCheckpointing(10_000);
+
+        //        DataStream<String> stream =
+        // env.readTextFile("s3://shausma-nyc-tlc/yellow-trip-data/taxi-trips.json/dropoff_year=2010/part-00000-cdac5fe4-b823-4576-aeb7-7327b077476e.c000.json");
+
+        Properties consumerConfig = new Properties();
+        consumerConfig.put("aws.region", "eu-west-1");
+        consumerConfig.put("aws.credentials.provider", "AUTO");
+        consumerConfig.put("flink.stream.initpos", "TRIM_HORIZON");
+
+        DataStream<String> fromGen =
+                env.addSource(
+                        new RichSourceFunction<String>() {
+                            private static final long serialVersionUID = 1L;
+                            private volatile boolean running = true;
+                            private volatile boolean isRestored = false;
+                            private int emittedCount = 0;
+
+                            public void run(SourceContext<String> ctx) throws Exception {
+                                for (; this.running; Thread.sleep(1L)) {
+                                    synchronized (ctx.getCheckpointLock()) {
+                                        ctx.collect(String.valueOf(this.emittedCount));
+                                    }
+
+                                    if (this.emittedCount < 100) {
+                                        ++this.emittedCount;
+                                    } else {
+                                        this.emittedCount = 0;
+                                    }
+                                }
+                            }
+
+                            public void cancel() {
+                                this.running = false;
+                            }
+                        });
+
+        fromGen.map(
+                        x -> {
+                            System.out.println(x);
+                            return x;
+                        })
+                .sinkTo(new KinesisDataStreamsAsyncSink<>());
+
+        /*
+         * Here, you can start creating your execution plan for Flink.
+         *
+         * Start with getting some data from the environment, like
+         * 	env.readTextFile(textPath);
+         *
+         * then, transform the resulting DataStream<String> using operations
+         * like
+         * 	.filter()
+         * 	.flatMap()
+         * 	.join()
+         * 	.coGroup()
+         *
+         * and many more.
+         * Have a look at the programming guide for the Java API:
+         *
+         * https://flink.apache.org/docs/latest/apis/streaming/index.html
+         *
+         */
+
+        // execute program
+        env.execute("Flink Streaming Java API Skeleton");
+    }
+}
